95a92774b9ad6c40916ce449e70773d7344273d2
Beatrix <beatrixkys@gmail.com>
2024-07-16 13:38:15 +0800

    Change and update Web Page

--- src/test/java/steps/VysorWebSteps.java
+++ src/test/java/steps/VysorWebSteps.java
@@ -2,15 +2,14 @@ package steps;
 
 import cucumber.api.java.en.Given;
 import pages.VysorWebPage;
-import utils.MobileHandlers;
 
 public class VysorWebSteps {
+
+    VysorWebPage vysorWebPage = new VysorWebPage();
     @Given("I am on Vysor Web")
     public void onVysorWeb(){
-        VysorWebPage vysorWebPage = new VysorWebPage();
         vysorWebPage.validateFirstLoad();
         vysorWebPage.checkButtons();
-        //Check buttons
     }
 
 }
